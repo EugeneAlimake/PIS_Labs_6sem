6b3479cd9cb9cf30bc25e0039cd73e9654f09b08
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2023-03-02 15:17:57 +0300

    Add files via upload

---
+++ Лабораторная работа 1b/WebApplication1/WebApplication1/IISHandler1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace WebApplication1
+{
+    public class IISHandler1 : IHttpHandler
+    {
+        /// <summary>
+        /// Вам потребуется настроить этот обработчик в файле Web.config вашего 
+        /// веб-сайта и зарегистрировать его с помощью IIS, чтобы затем воспользоваться им.
+        /// см. на этой странице: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region Члены IHttpHandler
+        WebSocket socket;
+
+        public bool IsReusable
+        {
+            // Верните значение false в том случае, если ваш управляемый обработчик не может быть повторно использован для другого запроса.
+            // Обычно значение false соответствует случаю, когда некоторые данные о состоянии сохранены по запросу.
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            socket = context.WebSocket;
+          
+            while (socket.State == WebSocketState.Open)
+            {
+                Thread.Sleep(2000);
+                await Send(DateTime.Now.ToString("HH:mm:ss"));
+            }
+
+        }
+
+        private async Task Send(string s)
+        {
+            var sendbuffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes(s));
+            await socket.SendAsync(sendbuffer, //Сообщение HTTP-запроса для отправки.
+                WebSocketMessageType.Text,  //тип
+                true, CancellationToken.None//Токен отмены для отмены операции.
+                                                                       );
+        }
+
+        #endregion
+    }
+}
